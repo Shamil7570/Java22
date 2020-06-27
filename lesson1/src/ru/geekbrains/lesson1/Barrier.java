3b1aa428ffd02f799df2d8a04f10f7226d8fe769
Shamil7570 <63641940+Shamil7570@users.noreply.github.com>
2020-06-27 17:01:02 +0300

    Дз1

---
+++ lesson1/src/ru/geekbrains/lesson1/Barrier.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.lesson1;
+
+public abstract class Barrier {
+    private String name;
+
+    public Barrier(String name) {
+        this.name = name;
+    }
+
+    protected abstract boolean moving(Actions actions);
+
+    public String getName() {
+        return name;
+    }
+}
+
